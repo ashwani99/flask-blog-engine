b5d4be43e8083868f22431a5f82b061c73dabfce
Ashwani Gupta <ashwanigupta125@gmail.com>
2019-01-13 22:27:07 +0530

    Add working version: templates

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,18 @@
 from app import app
+from flask import render_template
 
 
 @app.route('/')
 def index():
-    return 'Hello, from Ashwani'
+    user = {'username': 'Ashwani'}
+    posts = [
+        {
+            'author': {'username': 'John'},
+            'body': 'Nothing useful to write here'
+        },
+        {
+            'author': {'username': 'Shakespeare'},
+            'body': 'What\'s in a name?'
+        }
+    ]
+    return render_template('index.html', title='Home - Flask Blog Engine', user=user, posts=posts)
