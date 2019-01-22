0baa2d7908ee9a820b60551db8e4a2fc8974d5f9
Ashwani Gupta <ashwanigupta125@gmail.com>
2019-01-22 22:22:03 +0530

    Add working version: forms, database

--- app/routes.py
+++ app/routes.py
@@ -1,8 +1,10 @@
 from app import app
-from flask import render_template
+from flask import render_template, redirect, flash, url_for
+from app.forms import LoginForm
 
 
 @app.route('/')
+@app.route('/index')
 def index():
     user = {'username': 'Ashwani'}
     posts = [
@@ -16,3 +18,14 @@ def index():
         }
     ]
     return render_template('index.html', title='Home - Flask Blog Engine', user=user, posts=posts)
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        flash('Login requested for user {}, remember_me={}'.format(
+            form.username.data, form.remember_me.data))
+        return redirect(url_for('index'))
+    print(dir(form))
+    return render_template('login.html', title='Sign In', form=form)
