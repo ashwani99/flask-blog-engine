b56b337ca2425f40f8f630f03c9a48b318df085b
Ashwani Gupta <ashwanigupta125@gmail.com>
2019-02-13 09:58:25 +0530

    add custom error handlers

--- app/__init__.py
+++ app/__init__.py
@@ -4,6 +4,10 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_login import LoginManager
 
+import logging
+from logging.handlers import SMTPHandler, RotatingFileHandler
+import os
+
 app = Flask(__name__)
 # print(dir(app))
 app.config.from_object(Config)
@@ -12,5 +16,26 @@ migrate = Migrate(app, db)
 login = LoginManager(app)
 login.login_view = 'login'
 
+if app.debug:
+    auth = None
+    if app.config['MAIL_USERNAME'] or app.config['MAIL_PASSWORD']:
+        auth = (app.config['MAIL_USERNAME'], app.config['MAIL_PASSWORD'])
+    secure = None
+    if app.config['MAIL_USE_TLS']:
+        secure = ()
+    mail_handler = SMTPHandler(
+        mailhost=(app.config['MAIL_SERVER'], app.config['MAIL_PORT']),
+        fromaddr='no-reply@' + app.config['MAIL_SERVER'],
+        toaddrs=app.config['ADMINS'], subject='MicroBlog Engine Failure',
+        credentials=auth, secure=secure)
+    mail_handler.setLevel(logging.ERROR)
+    app.logger.addHandler(mail_handler)
+    
 
-from app import routes, models
+    if not os.path.exists('logs'):
+        os.mkdir('logs')
+    file_handler = RotatingFileHandler('logs/flask-blog-engine.log', maxBytes=10240
+                                        backupCount=10)
+                                        
+
+from app import routes, models, errors
---
+++ app/errors.py
@@ -0,0 +1,14 @@
+from app import app, db
+from flask import render_template
+
+
+@app.errorhandler(404)
+def not_found_error(error):
+    return render_template('404.html'), 404
+
+
+@app.errorhandler(500)
+def internal_error(error):
+    print('500 OCCURED !!!')
+    db.session.rollback()
+    return render_template('500.html'), 500
