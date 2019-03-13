1f69efbafc5ea34510eef4007aaf4acc8a50e170
Ashwani Gupta <ashwanigupta125@gmail.com>
2019-03-13 11:32:52 +0530

    Windows workaround

--- app/__init__.py
+++ app/__init__.py
@@ -17,24 +17,24 @@ login = LoginManager(app)
 login.login_view = 'login'
 
 if app.debug:
-    auth = None
-    if app.config['MAIL_USERNAME'] or app.config['MAIL_PASSWORD']:
-        auth = (app.config['MAIL_USERNAME'], app.config['MAIL_PASSWORD'])
-    secure = None
-    if app.config['MAIL_USE_TLS']:
-        secure = ()
-    mail_handler = SMTPHandler(
-        mailhost=(app.config['MAIL_SERVER'], app.config['MAIL_PORT']),
-        fromaddr='no-reply@' + app.config['MAIL_SERVER'],
-        toaddrs=app.config['ADMINS'], subject='MicroBlog Engine Failure',
-        credentials=auth, secure=secure)
-    mail_handler.setLevel(logging.ERROR)
-    app.logger.addHandler(mail_handler)
+    # auth = None
+    # if app.config['MAIL_USERNAME'] or app.config['MAIL_PASSWORD']:
+    #     auth = (app.config['MAIL_USERNAME'], app.config['MAIL_PASSWORD'])
+    # secure = None
+    # if app.config['MAIL_USE_TLS']:
+    #     secure = ()
+    # mail_handler = SMTPHandler(
+    #     mailhost=(app.config['MAIL_SERVER'], app.config['MAIL_PORT']),
+    #     fromaddr='no-reply@' + app.config['MAIL_SERVER'],
+    #     toaddrs=app.config['ADMINS'], subject='MicroBlog Engine Failure',
+    #     credentials=auth, secure=secure)
+    # mail_handler.setLevel(logging.ERROR)
+    # app.logger.addHandler(mail_handler)
     
 
     if not os.path.exists('logs'):
         os.mkdir('logs')
-    file_handler = RotatingFileHandler('logs/flask-blog-engine.log', maxBytes=10240
+    file_handler = RotatingFileHandler('logs/flask-blog-engine.log', maxBytes=10240,
                                         backupCount=10)
                                         
 
