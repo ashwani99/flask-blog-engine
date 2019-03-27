a7447b90a60bdec166dd347da010a8cfcfdb0904
Ashwani Gupta <ashwanigupta125@gmail.com>
2019-03-27 16:55:38 +0530

    Add working version: dates and times

--- app/__init__.py
+++ app/__init__.py
@@ -5,6 +5,7 @@ from flask_migrate import Migrate
 from flask_login import LoginManager
 from flask_mail import Mail
 from flask_bootstrap import Bootstrap
+from flask_moment import Moment
 
 import logging
 from logging.handlers import SMTPHandler, RotatingFileHandler
@@ -20,6 +21,7 @@ login = LoginManager(app)
 login.login_view = 'login'
 mail = Mail(app)
 bootstrap = Bootstrap(app)
+moment = Moment(app)
 
 
 if app.debug:
