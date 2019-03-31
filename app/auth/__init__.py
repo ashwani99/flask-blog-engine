52e3b3683e87d52638a86a9c44795430a4c864bc
Ashwani Gupta <ashwanigupta125@gmail.com>
2019-03-31 20:41:45 +0530

    Add application factory function

--- app/auth/__init__.py
+++ app/auth/__init__.py
@@ -2,8 +2,6 @@ from flask import Blueprint
 from flask_login import LoginManager
 from app import app
 
-login = LoginManager(app)
-login.login_view = 'auth.login'
 bp = Blueprint('auth', __name__)
 
 from app.auth import email, routes, forms
