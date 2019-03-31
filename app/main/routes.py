3ebaf6204368d2fac9c5d1fdc838b8210f4185a2
Ashwani Gupta <ashwanigupta125@gmail.com>
2019-03-31 21:23:08 +0530

    Minor fix: filter all user posts in /explore

--- app/main/routes.py
+++ app/main/routes.py
@@ -97,7 +97,7 @@ def unfollow(username):
 @login_required
 def explore():
     page = request.args.get('page', 1, type=int)
-    posts = current_user.followed_posts().paginate(
+    posts = Post.query.order_by(Post.timestamp.desc()).paginate(
         page, current_app.config['POSTS_PER_PAGE'], False)
     next_url = url_for('explore', page=posts.next_num) \
         if posts.has_next else None
