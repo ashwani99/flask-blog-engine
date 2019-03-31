265d47ea3c68f73f052c0681944a376d33086843
Ashwani Gupta <ashwanigupta125@gmail.com>
2019-03-31 19:39:58 +0530

    Add  for auth blueprint

--- app/__init__.py
+++ app/__init__.py
@@ -27,7 +27,7 @@ from app.errors import bp as errors_bp
 app.register_blueprint(errors_bp)
 
 from app.auth import bp as auth_bp
-app.register_blueprint(auth_bp)
+app.register_blueprint(auth_bp, url_prefix='/auth')
 
 if app.debug:
     # Receiving errors via email
